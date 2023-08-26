899655fc3c986ab49da7cae3d826438032989193
Rashed Arman <rashedarman21@gmail.com>
2023-08-26 19:04:18 +0600

    create seed data for greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greeting1 = Greeting.create(
+  text: 'Greetings, traveler of the cosmos! May your journey through the stars be as wondrous as the birth of a new galaxy.'
+)
+greeting2 = Greeting.create(
+  text: 'Hello, sentient being of the 22nd century! Wishing you a glitch-free day in this digital realm of possibilities.'
+)
+greeting3 = Greeting.create(
+  text: 'Welcome to another day in the quantum realm! May your experiences be as unpredictable as the behavior of subatomic particles.'
+)
+greeting4 = Greeting.create(
+  text: 'Greetings, augmented human of the cyber age! May your interfaces be intuitive and your reality augmented with joy.'
+)
+greeting5 = Greeting.create(
+  text: 'Salutations, explorer of alternate dimensions! Wishing you a day as mind-bending as traversing through parallel realities.'
+)
